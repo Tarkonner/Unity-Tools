7931d78ae24ed9203ac31f9a419bcc91d5a08ab3
Tarkonner <anderslykketornbjerg@gmail.com>
2020-04-27 13:07:56 +0200

    Randomish audio

---
+++ AudioRandomPitch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioRandomPitch : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    
+    //Pitch
+    [Range(0, 3)] [SerializeField] private float minPitch = .9f;
+    [Range(0, 3)] [SerializeField] private float maxPitch = 1.1f;
+    
+    [Range(0, 1)] [SerializeField] private float minVolume = .9f;
+    [Range(0, 1)] [SerializeField] private float maxVolume = 1f;
+    
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+    
+
+    void RandomPitch()
+    {
+        _audioSource.pitch = Random.Range(minPitch, maxPitch);
+    }
+    
+    void RandomVolume()
+    {
+        _audioSource.volume = Random.Range(minVolume, maxVolume);
+    }
+}
