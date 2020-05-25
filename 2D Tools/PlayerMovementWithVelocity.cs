db751f385a1b4d8e6c9fd0ccbbf4bbdb0e8435cf
Tarkonner <anderslykketornbjerg@gmail.com>
2020-05-25 21:01:51 +0200

    Update PlayerMovementWithVelocity.cs

--- 2D Tools/PlayerMovementWithVelocity.cs
+++ 2D Tools/PlayerMovementWithVelocity.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,8 +10,18 @@ public class PlayerMovementWithVelocity : MonoBehaviour
     
     private Rigidbody2D _rb;
     private Vector2 _playerInput;
-
+    private Vector2 _lastInput;
+    
+    [Header("Speed")]
     [SerializeField] private float speed = 6.5f;
+    [SerializeField] private float timeToTopSpeed = .1f;
+
+    //Acceleration
+    private bool _topSpeed = false;
+    private float _acceleration;
+    private float _timeGone = 0;
+    private float _accelerationPercentage;
+    private bool _slowdownAcceleration;
     
     
     private void Awake()
@@ -19,21 +29,83 @@ public class PlayerMovementWithVelocity : MonoBehaviour
         _rb = GetComponent<Rigidbody2D>();
     }
 
-    
     void Update()
     {
+        //Control input
         if (!stopInputs)
         {
-            _playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+            _playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;    //Input
+            if (_playerInput != Vector2.zero) //Save last input
+            {
+                _lastInput = _playerInput;
+            }
         }
         else
         {
-            _playerInput = Vector2.zero;;
+            //What happens then the player don't have control.
+                //Slow down to stop
+            _playerInput = Vector2.zero;
+            _topSpeed = false;
+            _slowdownAcceleration = true;
         }
+        
+        //Control how fast the player move
+        if (_playerInput != Vector2.zero && !_topSpeed)     //Run-up acceleration
+        {
+            _timeGone += Time.deltaTime;
+                
+            _slowdownAcceleration = false;
+                
+            if (_timeGone < timeToTopSpeed)
+            {
+                SpeedControl();
+            }
+            else
+            {
+                _topSpeed = true;
+            }
+        }
+        else if (_playerInput == Vector2.zero && _timeGone > 0)     //Slowdown acceleration
+        {
+            _timeGone -= Time.deltaTime;
+                
+            _topSpeed = false;
+            _slowdownAcceleration = true;
+
+            if (_timeGone > 0)
+            {
+                SpeedControl();
+            }
+            else
+            {
+                _timeGone = 0;
+                _acceleration = 0;
+            }
+        }
+    }
+
+    private void SpeedControl()
+    {
+        _accelerationPercentage = _timeGone / timeToTopSpeed;
+        _acceleration = speed * Mathf.Pow(_accelerationPercentage, 2);
     }
 
     private void FixedUpdate()
     {
-        _rb.velocity = _playerInput * speed;
+        if (_topSpeed)
+        {
+            _rb.velocity = _playerInput * speed;
+        }
+        else
+        {
+            if (!_slowdownAcceleration)
+            {
+                _rb.velocity = _playerInput * _acceleration;
+            }
+            else
+            {
+                _rb.velocity = _lastInput * _acceleration;
+            }
+        }
     }
 }
