b62ce9ee193da2d19e9bc721744aa1f23b453f60
Tarkonner <anderslykketornbjerg@gmail.com>
2020-05-25 20:58:46 +0200

    Add files via upload

---
+++ BaseHealthSystem/PlayerHealth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : BaseHealthSystemWithInvincibilityFrames
+{
+    //Event then taking damage
+    public delegate void callTakingDamage();
+    public static event callTakingDamage OnDamage;
+    
+    public override void Damage(int damageTaken)
+    {
+        base.Damage(damageTaken);
+        OnDamage?.Invoke();
+    }
+}
---
+++ BaseHealthSystem/TextHealthbar.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextHealthbar : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI textForMaxHealth, textForMinHealth;
+
+    private GameObject _player;
+    private BaseHealthSystem _playerHealthSystem;
+    private bool _playerIsActive = false;
+
+    private int _savedHealth;
+
+    private void OnEnable()
+    {
+        PlayerHealth.OnDamage += ShowCurrentHealth;
+    }
+
+    private void OnDisable()
+    {
+        PlayerHealth.OnDamage -= ShowCurrentHealth;
+    }
+
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+
+        if (_player != null)
+        {
+            _playerIsActive = true;
+            _playerHealthSystem = _player.GetComponent<BaseHealthSystem>();
+        }
+        else
+        {
+            Debug.LogError("Here is no player, or it has no tag");
+        }
+        
+        if (_playerIsActive)
+        {
+            ShowMaxHealth();
+            ShowCurrentHealth();
+        }
+    }
+    
+    void ShowCurrentHealth()
+    {
+        _savedHealth = _playerHealthSystem.currentHealth;
+        textForMinHealth.text = _savedHealth.ToString();
+    }
+    
+    void ShowMaxHealth()
+    {
+        string showText = _playerHealthSystem.maxHealth.ToString() + " /";
+        textForMaxHealth.text = showText;
+    }
+}
