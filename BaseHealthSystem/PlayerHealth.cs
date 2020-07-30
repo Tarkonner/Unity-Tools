227fd4a90ef71e186ce27b19e93dcdc5ff1a09e8
Tarkonner <anderslykketornbjerg@gmail.com>
2020-07-30 11:35:11 +0200

    Update PlayerHealth.cs

--- BaseHealthSystem/PlayerHealth.cs
+++ BaseHealthSystem/PlayerHealth.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHealth : BaseHealthSystemWithInvincibilityFrames
+public class PlayerHealth : InvisFarmeHealth 
 {
     //Event then taking damage
     public delegate void callTakingDamage();
     public static event callTakingDamage OnDamage;
     
-    public override void Damage(int damageTaken)
+    public override void Interaction(int damageTaken)
     {
         base.Damage(damageTaken);
         OnDamage?.Invoke();
