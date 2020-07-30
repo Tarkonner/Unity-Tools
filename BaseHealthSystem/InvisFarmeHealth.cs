d5366985663d4e6d7dcea0ff0f0d7073e9edaf1a
Tarkonner <anderslykketornbjerg@gmail.com>
2020-07-30 11:33:30 +0200

    Add files via upload

---
+++ BaseHealthSystem/InvisFarmeHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisFarmeHealth : BaseForHealthSystem
+{
+    [SerializeField] private float invulnerabilityFrames = .2f;
+    private bool _isInvulnerable;
+    
+    public override void Interaction(int damage)
+    {
+        if (!_isInvulnerable)
+        {
+            StartCoroutine(InvulFrames());
+            base.Interaction(damage);
+        }
+    }
+
+    IEnumerator InvulFrames()
+    {
+        _isInvulnerable = true;
+        yield return new WaitForSeconds(invulnerabilityFrames);
+        _isInvulnerable = false;
+    }
+}
