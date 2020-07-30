3e0ed740803928a9d4e7695928748260be9c033a
Tarkonner <anderslykketornbjerg@gmail.com>
2020-07-30 11:32:18 +0200

    Funktion of a health system
    have the functions a heatlh system need

---
+++ BaseHealthSystem/HealthFunction.cs
@@ -0,0 +1,54 @@
+﻿public class HealthFunction 
+{
+    private int _max;
+    public int current;
+    public bool living;
+    
+    //Constructor
+    public HealthFunction(int maxHealth)
+    {
+        _max = maxHealth;
+    }
+    
+    //Healing
+    public bool Healing(int heal)
+    {
+        if (current == _max || heal < 1)
+            return false;
+
+        if (heal + current >= _max)
+        {
+            current = _max;
+        }
+        else
+        {
+            current += heal;
+        }
+        
+        return true;
+    }
+
+    public void FullHeal()
+    {
+        current = _max;
+        living = true;
+    }
+
+    public void AddToMaxHealth(int increase)
+    {
+        _max += increase;
+        Healing(increase);
+    }
+    
+    //Damage
+    public void TakingDamage(int damage)
+    {
+        if(damage < 1) //Gate for negativ numbers 
+            return;
+        
+        current -= damage;
+
+        if (current <= 0)
+            living = false;
+    }
+}
