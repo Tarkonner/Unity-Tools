b4a8f6a2fc3df008f9e982d9522da2eb379eec6f
Tarkonner <anderslykketornbjerg@gmail.com>
2020-07-30 11:36:32 +0200

    Update LookAtMouse.cs

--- 2D Tools/LookAtMouse.cs
+++ 2D Tools/LookAtMouse.cs
@@ -10,18 +10,18 @@ public class LookAtMouse : MonoBehaviour
 
     void Awake()
     {
-        _camera =Camera.main;
+        _camera = Camera.main;
     }
 
     void Update()
     {
         //Variabels
         var position = transform.position;
-        _dir=((Vector2)_camera.ScreenToWorldPoint(Input.mousePosition)-(Vector2)position).normalized;
+        _dir = ((Vector2)_camera.ScreenToWorldPoint(Input.mousePosition)-(Vector2)position).normalized;
         //Set cannonsposition
-        transform.position=(Vector2)position+_dir;
+        transform.position = (Vector2)position+_dir;
         //Calculate
-        _degree=Mathf.Atan2(_dir.y,_dir.x)*Mathf.Rad2Deg; // Get degree
+        _degree = Mathf.Atan2(_dir.y,_dir.x)*Mathf.Rad2Deg; // Get degree
         Quaternion rotaQuaternion = Quaternion.AngleAxis(_degree,Vector3.forward); //Make quaternion in 2D
         //Set rotation
         transform.rotation = rotaQuaternion;
